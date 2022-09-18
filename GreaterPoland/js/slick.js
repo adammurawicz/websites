efc9a83bedd4e5941924430fa9b59600e004c938
adammurawicz <murawicz_adam@gmail.com>
2022-09-18 20:43:58 +0200

    end of Greater Poland webside

---
+++ GreaterPoland/js/slick.js
@@ -0,0 +1,18 @@
+
+$('.residences_boxes').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    mobileFirst: true,
+    responsive: [{
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    }],
+
+});
+          
